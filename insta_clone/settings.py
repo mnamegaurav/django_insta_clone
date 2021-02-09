122159cf23d488969f208d4c1083da33ff5d3fae
Gaurav Sharma <gaurav91297@gmail.com>
2021-02-09 22:57:30 +0530

    MEDIA_ROOT added and profile picture is not clearly being shown in the frontend

--- insta_clone/settings.py
+++ insta_clone/settings.py
@@ -123,8 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [BASE_DIR/'static_files',]
 MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 AUTH_USER_MODEL = 'user.User'
