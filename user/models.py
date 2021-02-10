2a9ab05773343e9be183ae390b1c9d177ea28fc0
Gaurav Sharma <gaurav91297@gmail.com>
2021-02-10 05:37:15 +0530

    Help text added on user fields

--- user/models.py
+++ user/models.py
@@ -12,7 +12,7 @@ GENDER_CHOICES = [
 
 class User(AbstractUser):
     picture = models.ImageField(upload_to='profile_pictures', null=True, blank=True)
-    full_name = models.CharField(max_length=100)
+    full_name = models.CharField(max_length=100,help_text='Help people discover your account by using the name you\'re known by: either your full name, nickname, or business name.')
     email = models.EmailField(unique=True)
 
     # extra not required fields
@@ -20,7 +20,7 @@ class User(AbstractUser):
     phone_number = models.CharField(max_length=20, null=True, blank=True)
     is_account_private = models.BooleanField(default=False)
     website = models.URLField(null=True, blank=True)
-    bio = models.TextField(null=True, blank=True)
+    bio = models.TextField(null=True, blank=True, help_text='Provide your personal information, even if the account is used for a business, a pet or something else. This won\'t be a part of your public profile.')
 
     first_name = None
     last_name = None
