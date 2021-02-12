b5f2dfa03fa46ffba15c652d6678293dc5ee6960
Gaurav Sharma <gaurav91297@gmail.com>
2021-02-12 14:43:48 +0530

    added /in/ before username in user urls

--- user/urls.py
+++ user/urls.py
@@ -6,6 +6,6 @@ from user.views import (
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path('<str:username>/', login_required(ProfileView.as_view()), name='profile_view'),
-    path('<str:username>/edit/', login_required(ProfileEditView.as_view()), name='profile_edit_view'),
+    path('in/<str:username>/', login_required(ProfileView.as_view()), name='profile_view'),
+    path('in/<str:username>/edit/', login_required(ProfileEditView.as_view()), name='profile_edit_view'),
 ]
